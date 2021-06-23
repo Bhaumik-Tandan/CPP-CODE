52c553d3bc65e82ed565cb12e9c6820213898a2e
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2021-06-23 18:59:02 +0530

    Test contest

--- tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
-for (auto i = vv1.begin(); i != vv1.end(); ++i)
-            cout << (*i)->data << " ";
-        cout << endl;
-        for (auto i = vv2.begin(); i != vv2.end(); ++i)
-            cout << (*i)->data << " ";
